4046eeaa8c28a940e19d43442c6820e7a1b0e71a
Beatriz Torres <beatriz.torres150200@gmail.com>
2018-03-16 18:34:51 -0300

    adicionando app/index.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import Home from './screens/Home';
@@ -11,27 +10,3 @@ EStyleSheet.build({
 
 export default () => <Home />;
 
-=======
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
->>>>>>> b9c0c6786117c4995b8c8b362be8ed142a7dd3a6
